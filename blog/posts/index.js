dcc53de5cc66bff28523cff092ce0b9a2b92dbb3
Thomas Martinez <tmartinez114@gmail.com>
2022-02-05 01:29:04 -0500

    set up posts index.js

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -1,10 +1,30 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
 
 const app = express();
+app.use(bodyParser.json());
 
-app.get('/posts', (req, res) => {});
+// will store posts created
+const posts = {};
 
-app.post('/posts', (req, res) => {});
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/posts', (req, res) => {
+  // generate random ID
+  const id = randomBytes(4).toString('hex');
+  const { title } = req.body;
+
+  posts[id] = {
+    id,
+    title,
+  };
+
+  // 201 means resource is created
+  res.status(201).send(posts[id]);
+});
 
 app.listen(4000, () => {
   console.log('Listening on 4000');
