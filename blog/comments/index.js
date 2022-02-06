c88a358af9c3c63a96d21d2ff1443152e3e7b937
Thomas Martinez <tmartinez114@gmail.com>
2022-02-05 23:41:00 -0500

    POST request handler for comments/index.js

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -5,9 +5,24 @@ const { randomBytes } = require('crypto');
 const app = express();
 app.use(bodyParser.json());
 
+const commentsByPostId = {};
+
 app.get('/posts/:id/comments', (req, res) => {});
 
-app.post('/posts/:id/comments', (req, res) => {});
+app.post('/posts/:id/comments', (req, res) => {
+  // Generate new comment ID
+  const commentId = randomBytes(4).toString('hex');
+  // Pull content from comment
+  const { content } = req.body;
+  // check to see if posts has array of comments already if null then give empty array
+  const comments = commentsByPostId[req.params.id] || [];
+  // push new comment to this array
+  comments.push({ id: commentId, content });
+
+  commentsByPostId[req.params.id] = comments;
+
+  res.status(201).send(comments);
+});
 
 app.listen(4001, () => {
   console.log('Listening on 4001');
