408fd2d18afce0ccbb54a213a49ae87dd6185ffb
Thomas Martinez <tmartinez114@gmail.com>
2022-02-06 18:43:05 -0500

    useEffect to show Posts in the console

--- blog/client/src/PostList.js
+++ blog/client/src/PostList.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const PostList = () => {
+  // pull the posts that are in objects
+  const [posts, setPosts] = useState({});
+
+  const fetchPosts = async () => {
+    const res = await axios.get('http://localhost:4000/posts');
+
+    setPosts(res.data);
+  };
+
+  // useEffect used to run code at specific times of life cycle of component
+  // this will run only when component is first displayed
+  useEffect(() => {
+    fetchPosts();
+  }, []); // empty array tells react to only run once
+
+  console.log(posts);
+
   return <div></div>;
 };
 
