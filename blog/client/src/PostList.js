72ae36ad62b57314749ae146261cc091b8bfcd97
Thomas Martinez <tmartinez114@gmail.com>
2022-02-06 19:23:40 -0500

    added CommentCreate to PostList and have the postId from PostList

--- blog/client/src/PostList.js
+++ blog/client/src/PostList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import CommentCreate from './CommentCreate';
 
 const PostList = () => {
   // pull the posts that are in objects
@@ -29,6 +30,7 @@ const PostList = () => {
       >
         <div className='card-body'>
           <h3>{post.title}</h3>
+          <CommentCreate postId={post.id} />
         </div>
       </div>
     );
