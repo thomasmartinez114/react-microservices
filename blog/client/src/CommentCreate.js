1b8201a9303f24b9ef118fd19bafe1628edb13fa
Thomas Martinez <tmartinez114@gmail.com>
2022-02-06 19:18:02 -0500

    CommentCreate logic completed

--- blog/client/src/CommentCreate.js
+++ blog/client/src/CommentCreate.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 // need to know Id of Post that will associate to the comment being made
 const CommentCreate = ({ postId }) => {
+  const [content, setContent] = useState('');
+
+  const onSubmit = async event => {
+    event.preventDefault();
+
+    await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+      content,
+    });
+
+    setContent('');
+  };
+
   return (
     <div>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className='form-group'>
           <label>New Comment</label>
-          <input className='form-contro' />
+          <input
+            value={content}
+            onChange={e => setContent(e.target.value)}
+            className='form-control'
+          />
         </div>
         <button className='btn btn-primary'>Submit</button>
       </form>
